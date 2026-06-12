microsoft/Spartan2

Add small-value sumcheck optimization (Algorithm 6)
This replaces expensive field multiplications with native integer
arithmetic during the first ℓ₀ rounds when polynomial values are
guaranteed small.

Key components:

Lagrange accumulator infrastructure (src/lagrange_accumulator/):
- LagrangeAccumulators: precomputed A_i(v, u) values for all rounds
- LagrangeIndex/LagrangePoint/LagrangeHatPoint: type-safe domain indices
- LagrangeBasisFactory: barycentric Lagrange basis with O(D) evaluation
- extend_to_lagrange_domain: batch extension from {0,1}^ℓ₀ to U_D^ℓ₀
- EqRoundFactor: tracks α = eq(τ_{<i}, r_{<i}) across rounds
- Csr: compressed sparse row storage (2 allocations vs N+1 for Vec<Vec>)

Performance optimizations:
- Thread-local SpartanThreadState eliminates per-iteration allocations
- Delayed modular reduction via SignedWideLimbs accumulators
- Skip binary betas (Az·Bz = Cz on {0,1}^n for satisfying witnesses)
- Batched eq-weighted binding in transition phase

API:
- SmallValue trait: WideMul + Copy + Zero + Add + Sub + Send + Sync
- SmallValueEngine<SV>: blanket impl consolidates field requirements
- prove_cubic_small_value<E, SV, const LB>: main entry point

The prove_cubic_small_value function produces identical proofs to the
standard prove_cubic_with_three_inputs, verified via equivalence tests.

src/big_num/mod.rs
@@ -27,7 +27,7 @@ mod limbs;
 // Re-exports: traits
 pub use delayed_reduction::DelayedReduction;
 pub use field_reduction_constants::{BarrettReductionConstants, FieldReductionConstants};
-pub use small_value_field::SmallValueField;
+pub use small_value_field::{SmallValue, SmallValueEngine, SmallValueField};
 // TODO: Remove unused imports after adding small-value sumcheck support (https://github.com/microsoft/Spartan2/pull/112)
 #[allow(unused_imports)]
 pub use wide_mul::WideMul;

microsoft/vega-prover

src/big_num/mod.rs
@@ -33,6 +33,7 @@ pub use field_reduction_constants::{
 pub use montgomery::MontgomeryLimbs;
 // TODO: Remove unused imports after adding small-value sumcheck support (https://github.com/microsoft/Spartan2/pull/112)
 #[allow(unused_imports)]
+pub use small_value_conversion::{SmallValue, SmallValueEngine, SmallValueField};
 pub use wide_mul::WideMul;
 
 // TODO: Remove unused imports after adding small-value sumcheck support (https://github.com/microsoft/Spartan2/pull/112)